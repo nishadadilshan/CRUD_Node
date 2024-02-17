16e20c164d96fd5811d1d5406a92a2d1188fa0e6
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-02-18 00:37:01 +0530

    Added nodemon to refresh the server when there are changes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
+});
+
 app.listen(3000, () => {
   console.log("Node api app is running on port 3000");
 });
