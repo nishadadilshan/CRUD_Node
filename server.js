58aae5e87b3a59a7e85f3ea17de8fa2747ff18f1
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-02-18 15:47:28 +0530

    added get products and get product by id

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Product = require("./models/productModels");
 
 app.use(express.json());
 
-//route
+//routes
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
@@ -14,7 +14,26 @@ app.get("/blog", (req, res) => {
   res.send("Hello Blog");
 });
 
-app.post("/product", async (req, res) => {
+app.get("/getProduct", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/getProductByID/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/addProduct", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
