dad2cfbf315d2239faba6d7dee67b29ad3e2c939
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-02-18 15:32:54 +0530

    created post request to add data to db

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/productModels");
+
+app.use(express.json());
 
 //route
 app.get("/", (req, res) => {
@@ -9,6 +12,16 @@ app.get("/", (req, res) => {
 
 app.get("/blog", (req, res) => {
   res.send("Hello Blog");
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.meesage });
+  }
 });
 
 mongoose
