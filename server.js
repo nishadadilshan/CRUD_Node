33bf6988720a807724a79b98fb56e3d38e33cb4e
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-02-18 13:39:20 +0530

    Added Mongoose to the project and connected with database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //route
 app.get("/", (req, res) => {
@@ -10,6 +11,16 @@ app.get("/blog", (req, res) => {
   res.send("Hello Blog");
 });
 
-app.listen(3000, () => {
-  console.log("Node api app is running on port 3000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://nishadadilshan:Dilshan123@cluster0.wqt3hza.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to the mongo DB");
+    app.listen(3000, () => {
+      console.log("Node api app is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
